154a198b8369ed4144815c6aadce69fe2bb13ed4
TRI THANH <trannguyentrithanh38@gmail.com>
2026-04-06 16:33:55 +0700

    feat: Add badge image for rewardNFT on Pinata

--- contracts/RewardNFT.sol
+++ contracts/RewardNFT.sol
@@ -1,12 +1,14 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.26;
 
-import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 // Ownable -> Dùng để phân quyền
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract RewardNFT is ERC721, Ownable {
+contract RewardNFT is ERC721URIStorage, Ownable {
     uint256 private _nextTokenId;
+
+    string public constant BADGE_URI = "ipfs://bafkreift5pjw2b4nrfzovrotxe4jakiqeivjrtgi44gs522wghed6xdltq";
 
     // Inialize NFT
     // initialOwner ref is address of contract crowdFund
@@ -18,5 +20,6 @@ contract RewardNFT is ERC721, Ownable {
     function mint (address to) external onlyOwner{
         uint256 tokenId = _nextTokenId++;
         _mint(to, tokenId);
+        _setTokenURI(tokenId, BADGE_URI);
     }
 }
